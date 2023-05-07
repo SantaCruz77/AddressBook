b88a0e003b66784c67cf6126144c082751606cd0
SantaCruz77 <steveyamada66@gmail.com>
2023-05-07 17:37:39 +0900

    顧客登録機能を実装する。

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\UpdateCustomerRequest;
+use App\Models\Customer;
+use Inertia\Inertia;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Inertia::render('Customers/Index', [
+            'customers' => Customer::select('id', 'name', 'postcode', 'address')->get()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Customers/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCustomerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCustomerRequest $request)
+    {
+        Customer::create([
+            'name' => $request->name,
+            'postcode' => $request->postcode,
+            'address' => $request->address,
+        ]);
+
+        return to_route('customers.index')
+            ->with([
+                'message' => '登録しました。',
+                'status' => 'success'
+            ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Customer $customer)
+    {
+        return Inertia::render('Customers/Show', [
+            'customer' => $customer
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateCustomerRequest  $request
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCustomerRequest $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Customer  $customer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
---
+++ resources/js/Pages/Customers/Create.vue
@@ -0,0 +1,83 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { reactive } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
+
+defineProps({
+    errors: Object
+})
+
+const form = reactive({
+    name: null,
+    postcode: null,
+    address: null,
+})
+
+const storeAddress = () => {
+    Inertia.post('/customers', form)
+}
+</script>
+
+<template>
+    <Head title="顧客登録" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">顧客登録</h2>
+        </template>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <section class="text-gray-600 body-font relative">
+                            
+                            <form @submit.prevent="storeAddress">
+                                <div class="container px-5 py-8 mx-auto">
+                                    <div class="lg:w-1/2 md:w-2/3 mx-auto">
+                                        <div class="flex flex-wrap -m-2">
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="name" class="leading-7 text-sm text-gray-600">
+                                                        顧客名
+                                                    </label>
+                                                    <input type="text" id="name" name="name" v-model="form.name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    <div v-if="errors.name" class="text-red-700">{{ errors.name }}</div>
+                                                </div>
+                                            </div>
+                                            
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="postcode" class="leading-7 text-sm text-gray-600">
+                                                        郵便番号
+                                                    </label>
+                                                    <input type="text" id="postcode" name="postcode" v-model="form.postcode" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    <div v-if="errors.postcode" class="text-red-700">{{ errors.postcode }}</div>
+                                                </div>
+                                            </div>
+
+                                            <div class="p-2 w-full">
+                                                <div class="relative">
+                                                    <label for="address" class="leading-7 text-sm text-gray-600">
+                                                        住所
+                                                    </label>
+                                                    <input type="text" id="address" name="address" v-model="form.address" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    <div v-if="errors.address" class="text-red-700">{{ errors.address }}</div>
+                                                </div>
+                                            </div>
+
+                                            <div class="p-2 w-full">
+                                                <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">顧客登録</button>
+                                            </div>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
